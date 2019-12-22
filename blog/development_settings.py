adef8d3bfb64d60ed313051607c823885ff7ee97
Tilman <tilmanherz03@gmail.com>
2019-12-22 14:34:46 +0800

    No django-guardian

--- blog/development_settings.py
+++ blog/development_settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'guardian',
     'ckeditor',
     'ckeditor_uploader',
     'imagekit',
