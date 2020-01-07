f45893c62328e320bee9e69a428999fa6017be57
Tilman <tilmanherz03@gmail.com>
2020-01-07 13:19:29 +0800

    Static files not loading in production

--- blog/settings.py
+++ blog/settings.py
@@ -130,7 +130,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'exchangeblog/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
