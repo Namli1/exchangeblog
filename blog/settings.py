5618d4feeb8b2ca769f782852f0fdd44847d3ffc
Namli <tilmanherz03@gmail.com>
2021-04-13 16:05:50 +0200

    Small settings update

--- blog/settings.py
+++ blog/settings.py
@@ -93,6 +93,11 @@ DATABASES = {
     }
 }
 
+# Automatically added primary key
+# https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
